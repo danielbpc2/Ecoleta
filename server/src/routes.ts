d64c595637444d0b08f017ed6156557ae038553b
danielbpc2 <danielbpc2@hotmail.com>
2020-09-09 00:16:14 -0300

    Feature: Controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,56 +1,21 @@
 import express from "express";
 import knex from "./database/connection";
 
+// Controllers
+import PointsController from "./controllers/pointsController";
+import ItemsController from "./controllers/itemsController";
+// Controllers Instances
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
 const routes = express.Router();
 
 // Listar itens de coleta
-routes.get("/items", async (request, response) => {
-  const items = await knex("items").select("*");
-  const serializedItems = items.map((item) => {
-    return {
-      id: item.id,
-      name: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-  return response.json(serializedItems);
-});
+routes.get("/items", itemsController.index);
 
-// TODO: Cadastrar pontos de coleta
-routes.post("/points", async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    items,
-  } = request.body;
+// Cadastrar pontos de coleta
+routes.post("/points", pointsController.create);
 
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx("points").insert({
-    image: "image-fake",
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-  });
-
-  const point_id = insertedIds[0];
-
-  const pointItems = items.map((item_id: number) => {
-    return { item_id, point_id };
-  });
-  await trx("points_items").insert(pointItems);
-
-  return response.json({ success: true });
-});
 // TODO: listar ponto de coleta especifico
 // TODO: listar pontos (Filtro por estado/cidade)
 export default routes;
