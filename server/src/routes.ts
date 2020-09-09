1367844226cc81060af9a9267903de86cf718ad3
danielbpc2 <danielbpc2@hotmail.com>
2020-09-08 22:59:01 -0300

    Feature: Create point Route almost done

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,6 +8,7 @@ routes.get("/items", async (request, response) => {
   const items = await knex("items").select("*");
   const serializedItems = items.map((item) => {
     return {
+      id: item.id,
       name: item.title,
       image_url: `http://localhost:3333/uploads/${item.image}`,
     };
@@ -16,6 +17,40 @@ routes.get("/items", async (request, response) => {
 });
 
 // TODO: Cadastrar pontos de coleta
+routes.post("/points", async (request, response) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    items,
+  } = request.body;
+
+  const trx = await knex.transaction();
+
+  const insertedIds = await trx("points").insert({
+    image: "image-fake",
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+  });
+
+  const point_id = insertedIds[0];
+
+  const pointItems = items.map((item_id: number) => {
+    return { item_id, point_id };
+  });
+  await trx("points_items").insert(pointItems);
+
+  return response.json({ success: true });
+});
 // TODO: listar ponto de coleta especifico
 // TODO: listar pontos (Filtro por estado/cidade)
 export default routes;
