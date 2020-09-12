5fbc8740948d6e23919f4156ce71080931f5963e
danielbpc2 <danielbpc2@hotmail.com>
2020-09-12 15:40:09 -0300

    Frontend -> Api items

--- frontend-Web/src/pages/CreatePoint/index.tsx
+++ frontend-Web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { Link } from "react-router-dom";
 
@@ -7,7 +7,21 @@ import { FiArrowLeft } from "react-icons/fi";
 
 import { Map, TileLayer, Marker } from "react-leaflet";
 
+import api from "../../services/api";
+
+interface Item {
+  id: number;
+  name: string;
+  image_url: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  useEffect(() => {
+    api.get("items").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
   return (
     <div id="page-create-point">
       <header>
@@ -80,30 +94,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oléo de cozinha</span>
-            </li>
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oléo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oléo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oléo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oléo de cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="" />
-              <span>Oléo de cozinha</span>
-            </li>
+            {items.map(({ id, name, image_url }) => (
+              <li key={id}>
+                <img src={image_url} alt="" />
+                <span>{name}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
         <button type="submit">Cadastrar ponto de coleta</button>
